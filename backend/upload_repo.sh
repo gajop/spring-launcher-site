9278963cd378576c1e0e50e1360a51292107e26a
Gajo Petrovic <gajo01@gmail.com>
2018-11-28 02:25:10 +0900

    set public permissions when uploading

--- backend/upload_repo.sh
+++ backend/upload_repo.sh
@@ -6,4 +6,4 @@
 echo "Copying $1/dist/*.json $1/dist/*.yml $1/dist/mac/*.zip $1/dist/mac/*.dmg $1/dist/*.exe $1/dist/*.AppImage to s3://spring-launcher/$2"
 
 s3cmd put $1/dist/*.json $1/dist/*.yml $1/dist/mac/*.zip $1/dist/mac/*.dmg \
-          $1/dist/*.exe $1/dist/*.AppImage s3://spring-launcher/$2/ --recursive
+          $1/dist/*.exe $1/dist/*.AppImage s3://spring-launcher/$2/ --recursive --acl-public --add-header=Cache-Control:max-age=86400
